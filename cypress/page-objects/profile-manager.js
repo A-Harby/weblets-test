8c5edd6d16c063d265bd63d9d973fa7443cc2c21
A-Harby <harbya@incubaid.com>
2023-04-03 13:50:12 +0200

    fix

--- cypress/page-objects/profile-manager.js
+++ cypress/page-objects/profile-manager.js
@@ -1,111 +1,49 @@
-class ProfileManager{
+class ProfileManager {
+  get getProfileManagerBtn() {
+    return cy.get("tf-profiles");
+  }
 
-    get getProfileManagerBtn(){
-       return cy.get('tf-profiles')
-       .shadow()
-       .find('.profile-menu')
-    }
+  get getMnemonicsField() {
+    return cy.get("tf-profiles").shadow().find("input[placeholder='Mnemonics']");
+  }
 
-    get getCreateProfileBtn(){
-        return cy.get('tf-profiles')
-        .shadow()
-        .find('.tabs')
-        .contains('Create Profile Manager')
-    }
+  get getPublicSShKeyField() {
+    return cy.get("tf-profiles").shadow().find("textarea");
+  }
 
-    get getProfilePasswordField(){
-        return cy.get('tf-profiles')
-        .shadow()
-        .find('.input')
-    }
+  get getProfileCloseButton() {
+    return cy.get("tf-profiles").shadow().find("button").contains("Close");
+  }
 
-    get getCreateNewProfileManagerBtn(){
-        return cy.get('tf-profiles')
-        .shadow()
-        .find('.button')
-    }
+  get getBody() {
+    return cy.get("body");
+  }
 
-    get getProfileNameField(){
-        return cy.get('tf-profiles')
-        .shadow()
-        .find(".input[placeholder='Profile Name']")
-        .clear({force: true})
-    }
+  ActivateProfileManager(mnemonics, sshKey) {
+    //Open the profile manager
+    this.getProfileManagerBtn.click();
 
-    get getMnemonicsField(){
-        return cy.get('tf-profiles')
-        .shadow()
-        .find("input[placeholder='Enter Your Mnemonics']")
-    }
+    //Add Mnemonics
+    this.getMnemonicsField.clear().type(mnemonics, { force: true });
 
-    get getPublicSShKeyField(){
-        return cy.get('tf-profiles')
-        .shadow()
-        .find("input[placeholder='Your public SSH key will be added as default to all deployments.']")
-    }
-    
-    get getActivateProfileButton(){
-        return cy.get('tf-profiles')
-        .shadow()
-        .find("button[class='button']")
-    }
+    //Add SSH Key
+    this.getPublicSShKeyField.clear().type(sshKey, { force: true });
 
-    get getDeactivateProfileBtn(){
-        return cy.get('tf-profiles')
-        .shadow()
-        .find("div:nth-child(3) > section:nth-child(1) > div:nth-child(1) > div:nth-child(1)")
-        .contains('Deactivate')
-    }
+    //Click outside the modal to close it
+    this.getProfileCloseButton.click();
+  }
 
-    get getBody(){
-        return cy.get('body')
-    }
+  //Deactivate the Profile Manager
+  DeactivateProfile() {
+    //Open the profile manager
+    this.getProfileManagerBtn.click();
 
+    //Deactivate the profile manager
+    this.getMnemonicsField.clear();
 
-    ActivateProfileManager(mnemonics, sshKey, profileSecret, vmName){
-        
-        //Open the profile manager
-        this.getProfileManagerBtn.click()
-
-        //Create a new profile manager
-        this.getCreateProfileBtn.click()
-
-        //Create a secret for the profile
-        this.getProfilePasswordField.type(profileSecret,{force: true})
-
-        //Confirm the secret
-        this.getCreateNewProfileManagerBtn.click()
-
-        //Change the profile name
-        this.getProfileNameField.type(vmName,{force: true})
-
-        //Add Mnemonics
-        this.getMnemonicsField.type(mnemonics,{force: true})
-
-        //Add SSH Key
-        this.getPublicSShKeyField.type(sshKey,{force: true})
-        
-        //Activate The Profile
-        this.getActivateProfileButton.click()
-
-        //Click outside the modal to close it
-        this.getBody.click(0,0)
-    }
-
-
-    //Deactivate the Profile Manager
-    DeactivateProfile(){
-
-        //Open the profile manager
-        this.getProfileManagerBtn.click()
-
-        //Deactivate the profile manager
-        this.getDeactivateProfileBtn.click()
-
-        //Click outside the modal to close it
-        this.getBody.click(0,0)
-    }
-
+    //Click outside the modal to close it
+    this.getProfileCloseButton.click();
+  }
 }
 
 export default new ProfileManager();
